455a8f0b9d6c0c182592e3dcdf96c2797df10788
Egor Dorichev <egordorichev@gmail.com>
2018-09-15 17:14:06 +0300

    Working on sprite transparencity

--- api.lua
+++ api.lua
@@ -16,8 +16,10 @@ end
 
 tsab_destroy = function() end
 
+printh = print
+
 tsab_error = function(error)
-	print("tsab_error: " .. error)
+	printh("tsab_error: " .. error)
 	exit()
 end
 
@@ -33,9 +35,15 @@ tsab_resize = function(w, h)
   y = (h - s * 128) / 2
 end
 
+local shape_shader
+local sprite_shader
+local default_shader
+
 tsab_draw = function()
   tsab_graphics_set_canvas(canvas)
+  tsab_shaders_set(default_shader)
   _draw()
+  tsab_shaders_set(-1)
   tsab_graphics_set_canvas(-1)
   tsab_graphics_draw(canvas, x, y, 0, 0, 0, s, s)
 end
@@ -152,7 +160,66 @@ end
 
 camera = tsab_graphics_camera
 title = tsab_graphics_set_title
-cls = tsab_graphics_clear
+
+cls = function(c)
+  if c then
+    c = math.floor(c) % 16
+    local p = palette[c]
+    tsab_graphics_clear(p[1], p[2], p[3])
+  else
+    tsab_graphics_clear()
+  end
+end
+
+local function parse_image(str)
+  local img = tsab_graphics_new_image(str)
+  local w, h = tsab_graphics_get_size(img)
+
+  tsab_graphics_set_canvas(img)
+
+  for y = 0, h - 1 do
+    for x = 0, w - 1 do
+      local r, g, b, a = tsab_graphics_get_pixel(img, x, y)
+
+      if a == 1 then
+        local ci, cs = 0, 1000
+
+        for i = 0, 15 do
+          local p = palette[i]
+          local s = (r - p[1]) + (g - p[2]) + (b - p[3])
+
+          if s > cs then
+            ci = i
+            cs = s
+          end
+        end
+
+        tsab_graphics_color(ci / 16, 0, 0, 1)
+        tsab_graphics_point(x, y)
+      else
+        pset(x, y, 0)
+      end
+    end
+  end
+
+  tsab_graphics_color()
+  tsab_graphics_set_canvas(-1)
+
+  return img
+end
+
+local sprites = parse_image("sprites.png")
+
+function spr(i, x, y, w, h)
+  if not sprites then return end
+
+  local sx = i % 16 * 8
+  local sy = i // 16 * 8
+
+  tsab_shaders_set(sprite_shader)
+  tsab_graphics_draw(sprites, x, y, 0, 0, 0, 1, 1, sx, sy, 8 * (w or 1), 8 * (h or 1))
+  tsab_shaders_set(defaut_shader)
+end
 
 --
 -- math
@@ -163,3 +230,45 @@ ceil = math.ceil
 function cos(x) return math.cos((x or 0)*(math.pi*2)) end
 function sin(x) return math.sin(-(x or 0)*(math.pi*2)) end
 function atan2(x,y) return (0.75 + math.atan2(x,y) / (math.pi * 2)) % 1.0 end
+
+--
+-- shaders
+--
+
+shape_shader = tsab_shaders_new([[
+varying vec2 texCoord;
+uniform vec4 color;
+uniform float palette[16];
+
+void main() {
+  gl_FragColor = color;
+}
+]], true)
+
+sprite_shader = tsab_shaders_new([[
+varying vec2 texCoord;
+uniform sampler2D tex;
+void main() {
+  gl_FragColor = texture2D(tex, texCoord);
+}
+]], true)
+
+default_shader = tsab_shaders_new([[
+varying vec2 texCoord;
+uniform sampler2D tex;
+uniform vec4 color;
+uniform float textured;
+void main() {
+    if (textured > 0.5) {
+        gl_FragColor = texture2D(tex, texCoord) * color;
+    } else {
+        gl_FragColor = color;
+    }
+}
+]], true)
+
+tsab_shaders_set(default_shader)
+tsab_shaders_send_vec4(default_shader, "color", 1, 1, 1, 1)
+tsab_shaders_set(shape_shader)
+tsab_shaders_send_vec4(shape_shader, "color", 1, 1, 1, 1)
+tsab_shaders_set(-1)
--- main.lua
+++ main.lua
@@ -1,5 +1,11 @@
+function _init()
+  --palt(0, false)
+  --palt(14, true)
+end
+
 function _draw()
   cls()
+  --[[
   local t = time() / 4
   local am = 10
 
@@ -9,6 +15,10 @@ function _draw()
     local x = cos(a) * d + 64
     local y = sin(a) * d + 64
 
-    circ(x, y, 3, i % 2 == 0 and 8 or 13)
+    spr(16, x, y)
+    --circ(x, y, 4, i % 2 == 0 and 2 or 1)
+    --circ(x, y, 3, i % 2 == 0 and 8 or 13)
   end
+  ]]
+  spr(0, 0, 0, 16, 16)
 end
